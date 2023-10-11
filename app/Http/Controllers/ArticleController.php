f35837e2810ee5e52587289960c41b5d06b29c77
BinAref <a.barakani.a@gmail.com>
2023-10-11 19:40:57 +0300

    ArticleController and migrate

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all();
+        return response()->json($articles);
+    }
+
+    public function show($id)
+    {
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json(['error' => 'Article not found'], 404);
+        }
+
+        return response()->json($article);
+    }
+}
