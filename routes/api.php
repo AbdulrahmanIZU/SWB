847a2a0fdf23adf776f567839af3fcf05f7ab18a
BinAref <a.barakani.a@gmail.com>
2023-10-11 19:28:03 +0300

    make routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// index is a mothed that will be in the controller file and it will return a list of all articles in a JSON format
+Route::get('/articles', [ArticleController::class, 'index']);
+
+// show is a mothed that will be in the controller file and it will retrieves and returns details of a single article based on its ID in a JSON format.
+Route::get('/article/{id}', [ArticleController::class, 'show']);
