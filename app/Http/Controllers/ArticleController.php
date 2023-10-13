29bddd0f9fb738a06c1419ec4b30d58f56b541a5
BinAref <a.barakani.a@gmail.com>
2023-10-13 20:03:43 +0300

    useing faker

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,23 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Faker\Factory as Faker;
 
 class ArticleController extends Controller
 {
+
     public function index()
     {
-        $articles = Article::all();
+        $faker = Faker::create();
+        $articles = [];
+    
+        for ($i = 0; $i < 10; $i++) {
+            $articles[] = [
+                'id' => $i + 1,
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+                'created_at' => $faker->dateTime,
+                'updated_at' => $faker->dateTime,
+            ];
+        }
+    
         return response()->json($articles);
     }
+    
 
     public function show($id)
-    {
-        $article = Article::find($id);
+{
+    $faker = Faker::create();
+    $article = [
+        'id' => $id,
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'created_at' => $faker->dateTime,
+                'updated_at' => $faker->dateTime,
+    ];
 
-        if (!$article) {
-            return response()->json(['error' => 'Article not found'], 404);
-        }
+    return response()->json($article);
+}
 
-        return response()->json($article[$id-1]);
-    }
 }
